b5771154e9511c7eef53d943b0c3a01b4aaa4cc6
SmrutiRanjanPahi <151525329+SmrutiRanjanPahi@users.noreply.github.com>
2025-07-11 17:07:46 +0530

    Update init_DB.sql

--- Scripts/init_DB.sql
+++ Scripts/init_DB.sql
@@ -1,3 +1,10 @@
+/*This script creates new Databases Named 'DW_Bonze', 'DW_Silver', 'DW_Gold' after checking if they already exist.
+If the databases exist , it drops them and recreate the databases.
+WARNING: 
+Running this script will Drop all the Databases if exist.
+All data inside the Databases will be deleted permanently.
+Proceed with Caution and ensure That it is safe to drop the databases or the data inside have proper backup before running this script.
+*/ 
 Drop Database if exists DW_Bronze; -- Drop the Database If the Database 'DW_Bronze' is already Exist 
 create Database DW_Bronze; -- Create Database named DW_Bronze
 Drop Database if exists DW_Silver; -- Drop the Database If the Database 'DW_Silver' is already Exist 
